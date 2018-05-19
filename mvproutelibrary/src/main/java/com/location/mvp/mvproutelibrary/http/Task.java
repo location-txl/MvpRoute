3bcef34ed05de00f6f00ae7175ca43a6b7a85ab9
TLocation <tttx0307@163.com>
2018-05-20 00:30:37 +0800

    13

---
+++ mvproutelibrary/src/main/java/com/location/mvp/mvproutelibrary/http/Task.java
@@ -0,0 +1,15 @@
+package com.location.mvp.mvproutelibrary.http;
+
+/**
+ * 项目名称: MvpRoute
+ * 类描述:
+ * 创建人: 田晓龙
+ * 创建时间: 2018/5/20 0020 0:12
+ * 修改人:
+ * 修改内容:
+ * 修改时间:
+ */
+
+
+public class Task {
+}
