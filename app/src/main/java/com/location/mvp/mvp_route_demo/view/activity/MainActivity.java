7556391480f0b0f8e13e32576146926b8fdea978
TLocation <tttx0307@163.com>
2018-05-24 23:51:44 +0800

    更改mannfist

--- app/src/main/java/com/location/mvp/mvp_route_demo/view/activity/MainActivity.java
+++ app/src/main/java/com/location/mvp/mvp_route_demo/view/activity/MainActivity.java
@@ -1,13 +1,14 @@
 package com.location.mvp.mvp_route_demo.view.activity;
 
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.location.mvp.mvp_route_demo.R;
 import com.location.mvp.mvp_route_demo.presenter.MainPresenter;
 import com.location.mvp.mvp_route_demo.view.fragment.TestFragment;
 import com.location.mvp.mvproutelibrary.Base.BaseActivity;
 import com.location.mvp.mvproutelibrary.Base.BaseThrowable;
-import com.location.mvp.mvproutelibrary.utils.FragmentUtils;
 
 public class MainActivity extends BaseActivity<MainPresenter> {
 
@@ -19,13 +20,11 @@ public class MainActivity extends BaseActivity<MainPresenter> {
 
 	@Override
 	protected void initView() {
-//        FragmentManager supportFragmentManager = getSupportFragmentManager();
-//        FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
-//        TestFragment testFragment = new TestFragment();
-//        fragmentTransaction.add(R.id.id_fre, testFragment);
-//        fragmentTransaction.commit();
-		FragmentUtils.getInstance(this)
-				.start(TestFragment.class);
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+        TestFragment testFragment = new TestFragment();
+        fragmentTransaction.add(R.id.id_fre, testFragment);
+        fragmentTransaction.commit();
 	}
 
 	@Override
