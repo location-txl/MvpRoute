8d2b8fd49255a43654e3e751fe5452013e0839bd
TLocation <tttx0307@163.com>
2018-05-27 18:17:01 +0800

    log日志第一版优化完成

---
+++ mvproutelibrary/src/main/java/com/location/mvp/mvproutelibrary/utils/Denutils.java
@@ -0,0 +1,30 @@
+package com.location.mvp.mvproutelibrary.utils;
+
+import android.content.Context;
+
+/**
+ * 项目名称: MvpRoute
+ * 类描述:
+ * 创建人: 田晓龙
+ * 创建时间: 2018/5/27 0027 13:58
+ * 修改人:
+ * 修改内容:  尺寸转换类
+ * 修改时间:
+ */
+
+
+public class Denutils {
+
+
+    public static float dptopx(Context context, float dp) {
+        return 1f;
+    }
+
+    public static float sptopx(Context context, float dp) {
+        return 1f;
+    }
+
+    public static float pxtodp(Context context, float dp) {
+        return 1f;
+    }
+}
